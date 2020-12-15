a9755f3e4180ae66000bce56c0453802c15ebe28
Giuseppe Guarino <gu.stuff@me.com>
2020-12-15 04:04:13 +0100

    Update aliases.zsh

--- aliases/aliases.zsh
+++ aliases/aliases.zsh
@@ -1,26 +1,11 @@
-# Git
-alias gc="git checkout"
-alias gst="git status"
-alias gpo="git push origin"
-alias gm="git merge"
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-alias wip="git add . && git commit -m 'wip'"
-alias nah="git reset --hard && git clean -df"
-
-# PHP
-alias p="phpunit"
-alias pf="phpunit --filter "
-alias art="php artisan"
-alias tinker="php artisan tinker"
-alias migrate="php artisan migrate"
-alias mfresh="php artisan migrate:fresh"
-alias mfs="php artisan migrate:fresh --seed"
-alias mrefresh="php artisan migrate:refresh"
-alias dumpload="composer dumpautoload"
-alias freshtest="php artisan migrate:fresh --env=testing"
-alias cfresh="rm -rf vendor/ composer.lock && composer i"
+# Shortcuts
+alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias reloadshell="source $HOME/.zshrc"
+alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
+alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+alias c="clear"
 alias path='echo -e ${PATH//:/\\n}'
-alias pest='nocorrect pest'
 
 # Flush Directory Service cache
 alias flushdns="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
@@ -28,3 +13,47 @@ alias flushdns="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
 # Empty the Trash on all mounted volumes and the main HDD
 # Also, clear Apple’s System Logs to improve shell startup speed
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
+
+# Directories
+alias dotfiles="cd $DOTFILES"
+alias library="cd $HOME/Library"
+alias sites="cd $HOME/Sites"
+
+# Git
+alias gst="git status"
+alias gb="git branch"
+alias gc="git checkout"
+alias gl="git log --oneline --decorate --color"
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+alias amend="git add . && git commit --amend --no-edit"
+alias commit="git add . && git commit -m"
+alias diff="git diff"
+alias force="git push --force"
+alias pop="git stash pop"
+alias pull="git pull"
+alias push="git push"
+alias resolve="git add . && git commit --no-edit"
+alias stash="git stash -u"
+alias unstage="git restore --staged ."
+
+alias wip="git add . && git commit -m 'wip'"
+alias nah="git reset --hard && git clean -df"
+
+# Laravel
+alias art="php artisan"
+alias pest='nocorrect pest'
+alias tinker="php artisan tinker"
+alias migrate="php artisan migrate"
+alias mfresh="php artisan migrate:fresh"
+alias mfs="php artisan migrate:fresh --seed"
+
+alias freshtest="php artisan migrate:fresh --env=testing"
+
+# PHP
+alias cfresh="rm -rf vendor/ composer.lock && composer i"
+alias dumpload="composer dumpautoload"
+
+# JS
+alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
+# alias watch="npm run watch"
